c403de71296fb0079b9b2d39e2bb1cff894f8110
David <david.tischer01@gmail.com>
2025-08-04 08:27:19 +0000

    Updated Config

--- src/testora/Config.py
+++ src/testora/Config.py
@@ -34,8 +34,8 @@ classification_prompt_version = 9
 # Start an automatic conversation with the llm
 # Only with this option set to True, corresponding issues
 # will be considered.
-ref_issues = True
-ref_comments = True
+ref_issues = False
+ref_comments = False
 
 automatic_chat = False
 
