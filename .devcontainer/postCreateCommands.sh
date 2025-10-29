dcd2befb8788fa74be9bb3b2bdb4c8ecabfd64a7
David <david.tischer01@gmail.com>
2025-10-29 10:20:17 +0000

    Changed project in the postCreateCommands.sh file

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -5,10 +5,10 @@ pip install -e .
 
 echo "Setting up project-under-analysis"
 # Select which project to analyze:
-# .devcontainer/setup_scipy.sh
+.devcontainer/setup_scipy.sh
 # .devcontainer/setup_pandas.sh
 # .devcontainer/setup_keras.sh
-.devcontainer/setup_marshmallow.sh
+# .devcontainer/setup_marshmallow.sh
 
 
 ## Experimental and not really supported as of now:
